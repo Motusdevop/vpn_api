41c6e78ba7c344fc94c7bf175609875bf684e616
Motusdevop <motusdevop@gmail.com>
2024-10-08 18:29:10 +0300

    fix

--- src/vpn/tools.py
+++ src/vpn/tools.py
@@ -15,11 +15,8 @@ def parse_clients(text: str) -> dict:
         if data[i].startswith(':::') and i != 0:
             break
 
-        if flag:
+        if i > 1:
             clients.append(data[i].split())
-
-        if data[i].startswith('Client'):
-            flag = True
 
     for line in data:
         if line.startswith('[disabled]'):
@@ -40,3 +37,18 @@ def parse_clients(text: str) -> dict:
         result['disabled_clients'].append({'config_name': item})
 
     return result
+
+if __name__ == '__main__':
+    text = '''::: Clients Summary :::
+Client         Public key                                        Creation date
+Motus          FwVG1oKSz/ncp0RoDqg9Stk0k3353jq/jHabMq5F83w=      04 Oct 2024, 15:45, UTC
+Motus-Mac      uCMGVM/Za2Q0gR8vVPh4rj9OovTJGRWb+5taoul2GnA=      04 Oct 2024, 15:52, UTC
+test           8GEmIPn889GTN7JUjqKPndJvzfjw1yWHUeswdxUsxw8=      05 Oct 2024, 16:47, UTC
+test2          wlmx/WmVisypMsCSkULOMq0qjQGPXWGwkIYv51yhQD8=      05 Oct 2024, 16:47, UTC
+user1          BaJUYPstbwWOKnI1TJzbPkOm3rMNF2UCxLcwi3vZajU=      05 Oct 2024, 16:47, UTC
+user2          pwKjs9HB9mMrDusWYzz19PTjHOAmPlmf6aeFI3fw1UE=      05 Oct 2024, 16:47, UTC
+::: Disabled clients :::
+[disabled]   test 
+[disabled]   test2 
+'''
+    print(parse_clients(text))
